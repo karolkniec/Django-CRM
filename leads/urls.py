2836f1bc986e54c0d40b2e327983a3b237ef3b11
Karol Knieć <karolkniec@outlook.com>
2023-04-05 15:50:31 +0200

    Assign agent to lead

--- leads/urls.py
+++ leads/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     lead_delete, lead_detail, lead_list, lead_create, lead_update, LeadListView, LeadDetailView, LeadCreateView,
     LeadUpdateView,
-    LeadDeleteView
+    LeadDeleteView,
+    AssignAgentView
 )
 
 app_name = "leads"
@@ -13,4 +14,5 @@ urlpatterns = [
     path('<int:pk>', LeadDetailView.as_view(), name='lead-detail'),
     path('<int:pk>/update', LeadUpdateView.as_view(), name='lead-update'),
     path('<int:pk>/delete', LeadDeleteView.as_view(), name='lead-delete'),
+    path('<int:pk>/assign-agent', AssignAgentView.as_view(), name='assign-agent')
 ]
